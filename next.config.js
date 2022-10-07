29b1b536ac02a1634f801434dfc1055649b72748
dumbledope <84159538+dumbledope@users.noreply.github.com>
2022-10-07 09:42:33 -0700

    feat: remove default cross-origin headers

--- next.config.js
+++ next.config.js
@@ -25,17 +25,21 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: '/api/:path*',
+        // matching all routes
+        source: '/*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
           },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
         ],
       },
     ];
